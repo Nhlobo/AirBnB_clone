648af42eccec98df5bdd71ff3a7dc48cc726a37d
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-10 12:46:15 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-
+"""
+BaseModel module for AirBnB clone project.
+"""
 from datetime import datetime
 import uuid
 
 class BaseModel:
+    """
+    The BaseModel class for handling initialization, serialization, and deserialization.
+    """
     def __init__(self):
-        # Your initialization logic here
-        pass
+        """
+        Initialization logic here.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        # Your string representation logic here
-        pass
+        """
+        String representation logic here.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        # Your save logic here
-        pass
+        """
+        Save logic here.
+        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        # Your to_dict logic here
-        pass
+        """
+        To_dict logic here.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
